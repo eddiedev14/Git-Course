cdec3d50d8ea6fdd4ffd164809624c15ecfe1857
Eddie Santiago <eddiesantiago122007@gmail.com>
2025-07-05 18:10:47 -0500

    Actualizar variable total

--- functions/totalShoppingCart.js
+++ functions/totalShoppingCart.js
@@ -1,8 +1,8 @@
 const preciosCarrito = [20, 30, 40, 1, 2, 5]
-let total = 0;
+let subtotal = 0;
 
 for (let i = 0; i < preciosCarrito.length; i++) {
-    total += preciosCarrito[i];
+    subtotal += preciosCarrito[i];
 }
 
 console.log(total)
